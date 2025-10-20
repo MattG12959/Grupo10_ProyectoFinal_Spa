e3ced0b8190ba4a652599f0ebe479db381b49b82
Matias Antonacci <matiasgantonacci@gmail.com>
2025-10-20 08:12:41 -0300

    Modificaciones
    Agregue un fondo de prueba.
    Agregue dos carpetas resources (img y sounds)
    Agregue en el Jfrom dos botones que funciona para reproducir la musica.

---
+++ src/resources/img/ImagenFondo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package resources.img;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+
+/**
+ *
+ * @author matia
+ */
+public class ImagenFondo extends JDesktopPane {
+    private Image imagen;
+
+    public ImagenFondo() {
+        // Carga la imagen desde la carpeta "img" (dentro de Source Packages)
+        imagen = new ImageIcon(getClass().getResource("/resources/img/fondo_flores.jpg")).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (imagen != null) {
+            // Dibuja la imagen escalada al tamaño actual del panel
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
