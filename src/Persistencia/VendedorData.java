37abec0ca9c2e47e1de72698b4add4a7f6c84ab2
NataliaDave <nataliadave@gmail.com>
2025-10-22 15:38:15 -0300

    VendedorData alta Lógica

--- src/Persistencia/VendedorData.java
+++ src/Persistencia/VendedorData.java
@@ -4,10 +4,43 @@
  */
 package Persistencia;
 
+import java.sql.Connection;
+import entidades.Vendedor;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
  */
 public class VendedorData {
-    
+     private Connection con = null;
+
+    public VendedorData(miConexion conexion) {
+        this.con = conexion.buscarConexion();
+        //con = miConexion.buscarConexion();
+    }
+      public void darAltaVendedor(int idEmpleado) {
+        String query = "UPDATE vendedor SET estado=1 WHERE idAlumno = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1, idEmpleado);
+            int i = ps.executeUpdate();
+
+            if (i == 0) {
+                JOptionPane.showMessageDialog(null, "Vendedor dado de alta con éxito.", "", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontro al vendedor", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(VendedorData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
