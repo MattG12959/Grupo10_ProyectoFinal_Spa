1c61bd261436a7a472b98d63edbbdc9f6ca2c201
Karina Altamirano <karina.altam@gmail.com>
2025-11-15 21:49:58 -0300

    Agrego clases TratamientosEsteticos y TratamientosRelajacion

---
+++ src/constantes/TratamientosEsteticos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constantes;
+
+/**
+ *
+ * @author usuario
+ */
+public enum TratamientosEsteticos {
+    DEPILACION_LASER("Depilación láser", "Eliminación permanente del vello mediante tecnología láser."),
+    DEPILACION_CERA("Depilación con cera", "Eliminación del vello mediante aplicación de cera caliente o fría."),
+    MANICURE("Manicure", "Cuidado y embellecimiento de las uñas de las manos."),
+    PEDICURE("Pedicure", "Cuidado y embellecimiento de las uñas de los pies."),
+    TRATAMIENTO_ANTIENVEJECIMIENTO("Tratamiento antienvejecimiento", "Tratamientos para reducir arrugas y mejorar la elasticidad de la piel."),
+    TRATAMIENTO_ANTICELULITICO("Tratamiento anticelulítico", "Tratamientos para reducir la apariencia de la celulitis."),
+    TRATAMIENTO_REDUCTOR("Tratamiento reductor", "Tratamientos para reducir medidas y tonificar el cuerpo."),
+    LIMPIEZA_PROFUNDA("Limpieza profunda", "Limpieza exhaustiva de la piel para eliminar impurezas y puntos negros.");
+    
+    // Campos de la clase, se ponen como privados y finales para que sean inmutables
+    private final String nombre;
+    private final String descripcion;
+
+    // Constructor que se llama para inicializar cada constante
+    TratamientosEsteticos(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    // Metodos getters para acceder a los campos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
---
+++ src/constantes/TratamientosRelajacion.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constantes;
+
+/**
+ *
+ * @author usuario
+ */
+public enum TratamientosRelajacion {
+    MASAJE_RELAJANTE("Masaje relajante", "Masaje suave que reduce el estrés y la tensión muscular, promoviendo la relajación general."),
+    AROMATERAPIA("Aromaterapia", "Uso de aceites esenciales para mejorar el bienestar físico y emocional."),
+    REFLEXOLOGIA("Reflexología", "Técnica de presión en puntos específicos de pies y manos para aliviar tensiones."),
+    REIKI("Reiki", "Técnica de sanación energética que promueve la relajación y el equilibrio."),
+    MEDITACION_GUIADA("Meditación guiada", "Sesiones de relajación mental y meditación para reducir el estrés."),
+    TERAPIA_CON_PIEDRAS_CALIENTES("Terapia con piedras calientes", "Masaje con piedras volcánicas calientes que relajan los músculos y mejoran la circulación."),
+    BAÑO_DE_VAPOR("Baño de vapor", "Sesión en baño turco o sauna para relajar y desintoxicar el cuerpo.");
+    
+    // Campos de la clase, se ponen como privados y finales para que sean inmutables
+    private final String nombre;
+    private final String descripcion;
+
+    // Constructor que se llama para inicializar cada constante
+    TratamientosRelajacion(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    // Metodos getters para acceder a los campos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
